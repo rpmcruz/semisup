d49e011b72320e164214641121fe568432c28944
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2025-03-04 07:55:17 +0000

    Rename variables. Separate supervised and unsupervised losses.

--- semisup.py
+++ semisup.py
@@ -1,16 +1,11 @@
 import torch, torchvision
 import copy
 
-class Supervised:
-    def __init__(self, model, weak_transform, strong_transform):
-        self.model = model
-        self.transform = weak_transform
-
-    def compute_loss(self, sup_imgs, sup_labels, unsup_imgs):
-        sup_imgs = self.transform(sup_imgs)
-        logits = self.model(sup_imgs)
-        supervised_loss = torch.nn.functional.cross_entropy(logits, sup_labels)
-        return supervised_loss, 0
+class Nop:
+    def __init__(self, *args, **kwargs):
+        pass
+    def __call__(self, unsup_imgs):
+        return 0
 
 class FixMatch:
     def __init__(self, model, weak_transform, strong_transform):
@@ -18,22 +13,16 @@ class FixMatch:
         self.weak_transform = weak_transform
         self.strong_transform = strong_transform
 
-    def compute_loss(self, sup_imgs, sup_labels, unsup_imgs, confidence=0.95):
-        # batch normalization is affected by doing two fpasses
-        # therefore make a single fpass
-        sup_imgs = self.weak_transform(sup_imgs)
-        weak_unsup_imgs = self.weak_transform(unsup_imgs)
-        strong_unsup_imgs = self.strong_transform(unsup_imgs)
-        logits = self.model(torch.cat((sup_imgs, weak_unsup_imgs, strong_unsup_imgs)))
-        sup_logits, weak_unsup_logits, strong_unsup_logits = torch.split(logits, (len(sup_imgs), len(weak_unsup_imgs), len(strong_unsup_imgs)))
-        # supervised loss
-        supervised_loss = torch.nn.functional.cross_entropy(sup_logits, sup_labels)
+    def __call__(self, imgs, confidence=0.95):
+        weak_imgs = self.weak_transform(imgs)
+        with torch.no_grad():
+            weak_logits = self.model(weak_imgs)
         # fixmatch loss (unsupervised)
-        weak_unsup_probs = weak_unsup_logits.detach().softmax(1)
-        max_probs, weak_unsup_labels = weak_unsup_probs.max(1)
+        weak_probs = weak_logits.detach().softmax(1)
+        max_probs, weak_labels = weak_probs.max(1)
         ix = max_probs >= confidence
-        unsupervised_loss = torch.nn.functional.cross_entropy(strong_unsup_logits[ix], weak_unsup_labels[ix]) if ix.sum() > 0 else 0
-        return supervised_loss, unsupervised_loss
+        strong_imgs = self.strong_transform(imgs[ix])
+        return torch.nn.functional.cross_entropy(self.model(strong_imgs), weak_labels[ix]) if ix.sum() > 0 else 0
 
 class MixMatch:
     # https://proceedings.neurips.cc/paper_files/paper/2019/hash/1cd138d0499a68f4bb72bee04bbec2d7-Abstract.html
@@ -41,23 +30,25 @@ class MixMatch:
         self.model = model
         self.weak_transform = weak_transform
 
-    def __call__(self, model, imgs, K=2, T=0.5, alpha=0.75):
+    def __call__(self, imgs, K=2, T=0.5, alpha=0.75):
         with torch.no_grad():
-            avg_probs = sum(torch.softmax(model(self.weak_augment(imgs)), 1) for _ in range(K)) / K
+            avg_probs = sum(self.model(self.weak_transform(imgs)).softmax(1) for _ in range(K)) / K
         # sharpen
         labels = (avg_probs**(1/T)) / torch.sum(avg_probs**(1/T))
         # mixup
         mixup = torchvision.transforms.v2.MixUp(num_classes=avg_probs.shape[1])
         imgs, labels = mixup(imgs, labels)
         # loss
-        probs = torch.softmax(model(imgs), 1)
+        probs = self.model(imgs).softmax(1)
         return torch.mean((probs - labels)**2)
 
+# FIXME: DINO results are bad; prolly not well implemented
 class DINO:
     # https://arxiv.org/abs/2104.14294
     def __init__(self, model, weak_transform, strong_transform):
         self.student = model
         self.teacher = copy.deepcopy(model)
+        self.weak_transform = weak_transform
 
     def __call__(self, imgs, teacher_momentum=0.999, student_temp=0.1, teacher_temp=0.04):
         # update teacher
@@ -69,4 +60,5 @@ class DINO:
         teacher_probs = torch.softmax((teacher_logits - teacher_logits.mean(0)) / teacher_temp, 1)
         student_logits = self.student(imgs)
         student_probs = torch.softmax(student_logits / student_temp, 1)
+        # loss
         return -torch.mean(torch.sum(teacher_probs * torch.log(student_probs+1e-9), 1))
